4d1b9971851eed39015f1fba764e58bef65c31d2
Roman <kap303777@mail.ru>
2022-07-12 18:28:39 +0300

    04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// Напишите программу, которая принимает на вход три числа и выдаёт 
+// максимальное из этих чисел.
 
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
@@ -9,6 +10,12 @@ Console.Write("Введите три числа: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 int num2 = Convert.ToInt32(Console.ReadLine());
 int num3 = Convert.ToInt32(Console.ReadLine());
+int Max = num1;
+if (num1 < num2) Max = num2;
+if (num2 < num3) Max = num3;
+Console.WriteLine($"максимальное число : {Max}");
+
+
 
 // int [] num = Convert.ToInt32(Console.ReadLine());
 // int size = 3;
