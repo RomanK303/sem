e9ad3d6006dd17b31de18fd2e02eb9db38714331
Roman <kap303777@mail.ru>
2022-07-25 16:15:36 +0300

    18

---
+++ Task17/Program.cs
@@ -0,0 +1,27 @@
+﻿// 17. Напишите программу, которая принимает на вход
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+// номер четверти плоскости, в которой находится эта
+// точка.
+Console.Clear();
+Console.WriteLine("Введите коордиинаты точки: ");
+Console.Write("X =  ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y = ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+// if (x > 0 && y > 0) Console.WriteLine("1 четверть");
+// else if (x < 0 && y > 0) Console.WriteLine("2 четверть");
+// else if (x < 0 && y < 0) Console.WriteLine("3 четверть");
+// else if (x > 0 && y < 0) Console.WriteLine("4 четверть");
+// else Console.WriteLine("Ошибка ввода координат");
+
+string Quarter(int xc, int yc) // формальные переменные
+{
+    if (xc > 0 && yc > 0) return "1 четверть";
+    if (xc < 0 && yc > 0) return "2 четверть";
+    if (xc < 0 && yc < 0) return "3 четверть";
+    if (xc > 0 && yc < 0) return "4 четверть";
+return "Ошибка ввода координат";
+}
+string result = Quarter(x,y); //фактические переменные в методе
+Console.WriteLine(result);
---
+++ Task18/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.Clear();
+Console.WriteLine("Введите номер координатной четверти: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+string Quarter(int xc) // формальные переменные
+{
+    if (xc == 1) return "X>0, Y>0";
+    if (xc == 2) return "X<0, Y>0";
+    if (xc ==3) return "X<0, Y<0";
+    if (xc ==4) return "X>0, Y<0";
+return "Ошибка ввода координат";
+}
+string result = Quarter (x); //фактические переменные в методе
+Console.WriteLine(result);
