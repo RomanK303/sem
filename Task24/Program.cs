15561b694a825a8e5b7da7ebd092d0b1cf897876
Roman <kap303777@mail.ru>
2022-08-02 20:45:08 +0300

    27,29

---
+++ Task24/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите число А: ");
+int num = int.Parse(Console.ReadLine());// Parse преоброзователь типа данных
+
+// int sum = 0;
+
+// for (int i = 1; i <= num; i++)
+// {
+//     sum += i;
+//     // sum = sum + i;
+// }
+
+// Console.WriteLine($"Сумма чисел от 1 до А: {sum}");
+// 1+2+3+4=10
+int Sum(int number)//Метод 
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int result = Sum(num);// возврат переменной
+Console.WriteLine($"Сумма чисел от 1 до А: {result}");
---
+++ Task25/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Clear();
+
+Console.WriteLine("Чтобы возвести число А в натуральную степень В:");
+Console.Write("1. Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("2. Введите целую степень В: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int Degree(int number, int degree)
+{
+    int result = 1;
+    if (degree > 0)
+    {
+        for (int i = 1; i <= degree; i++)
+        {
+            result = result * number;
+        }
+    }
+    else
+    {
+        for (int i = 1; i <= (-1 * degree); i++)
+        {
+            result = result * number;
+        }
+        result = 1 / result;
+    }
+    return result;
+}
+int result = Degree(a, b);
+Console.Write($"Число {a} в степень {b} = {result}");
+
